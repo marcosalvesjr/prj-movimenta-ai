841cd7609c3301a1c5a97295dc574645169075cd
Marcos Alves <mrcaurelio95@gmail.com>
2025-08-18 22:09:08 -0300

    refactor: improve modal scroll handling

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function clickToShow() {
   const modal = document.getElementById("modal");
   modal.style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 
 function clickToHide() {
   const modal = document.getElementById("modal");
   modal.style.display = "none";
+  document.body.style.overflow = "auto";
 }
